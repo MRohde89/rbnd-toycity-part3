b0f1954d23c112e079efeb4a5e633b78d45d0656
MRohde89 <rheinsberg1@gmx.net>
2016-02-02 07:30:21 +0100

    finished Products-Section. Next will be the Customer-Section

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,13 @@
 class Product
 
-  attr_reader :title, :item_array
-
+  attr_reader :title, :item_array, :stock, :price
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
@@ -29,6 +30,21 @@ class Product
     else @@products << self
     end
   end
+
+  def self.find_by_title(find_toy)
+    hash_of_toys = @@products.select { |toy| toy.title == find_toy}
+    return hash_of_toys[0] # because there will always be just one element inside
+  end
+
+  def in_stock?
+    self.stock != 0 ? true : false
+  end
+
+  def self.in_stock
+    return having_toys_in_stock = @@products.select {|toy| toy.stock != 0}
+    #return having_toys_in_stock.map { |not_in_stock_toy| not_in_stock_toy.title}
+  end
+
 
 
 
